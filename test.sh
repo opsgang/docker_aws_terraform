10c919f20a48443b1a11920910723ea6d2685ddc
jinal--shah <jnshah@gmail.com>
2018-02-19 00:00:15 +0000

    add check to test for specific version

--- test.sh
+++ test.sh
@@ -5,16 +5,11 @@ rc=0
 img=opsgang/${IMG:-aws_terraform}:candidate
 _c=test_tf
 
-t() {
-    local test_name="$1"
-    local cmd="$2"
-}
-
 test_name="default-with-preinstalled-plugins"
 echo "INFO $0: test $test_name"
 echo "INFO $0: ... should download versions as necessary."
 var="export TF_VAR_ts=$test_name"
-cmd="$var ; terraform init && terraform plan && terraform apply -auto-approve"
+cmd="$var ; terraform init && terraform apply -auto-approve"
 exp='Apply complete! Resources: 2 added, 0 changed, 0 destroyed.'
 docker rm -f $_c 2>/dev/null || true
 o=$(docker run --rm --name $_c -w /_test/default $img /bin/bash -c "$cmd")
@@ -30,11 +25,13 @@ test_name="try-preinstalled-copy-pre-0.10.7"
 echo "INFO $0: test $test_name"
 echo "INFO $0: ... should download versions as necessary."
 var="export TF_VAR_ts=$test_name"
-cmd="$var ; terraform init && terraform plan && terraform apply -auto-approve"
+cmd="$var ; terraform init && terraform apply -auto-approve"
 exp='Apply complete! Resources: 2 added, 0 changed, 0 destroyed.'
+v=0.10.6
+exp_v='Terraform v0\.10\.6$'
 docker rm -f $_c 2>/dev/null || true
-o=$(docker run --rm --name $_c -w /_test/default -e TERRAFORM_VERSION=0.10.6 $img /bin/bash -c "$cmd")
-if [[ $? -ne 0 ]] || [[ ! $(echo "$o" | grep "$exp") ]]; then
+o=$(docker run --rm --name $_c -w /_test/default -e TERRAFORM_VERSION=$v $img /bin/bash -c "$cmd")
+if [[ $? -ne 0 ]] || [[ ! $(echo "$o" | grep "$exp") ]] || [[ ! $(echo "$o" | grep -P "$exp_v") ]] ; then
     echo "ERROR $0: failure"
     echo "$o"
     rc=1
